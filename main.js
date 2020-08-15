05210f940da4579d2196d88793372aee05bbfabb
lfriel <friellaura10@gmail.com>
2020-08-15 12:30:52 -0400

    nav bar animation now using throttle

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('js connected');
 
-const NAV_SELECT_BUFFER = 500;
+const NAV_SELECT_BUFFER = 0;
+const THROTTLE_DELAY = 250;
 
 var lastSelection = document.getElementById('home');
 
@@ -16,27 +17,26 @@ function changeNavSelect(selection) {
 }
 */
 
-/*//cool throttle function. probably won't use but just in case
+//cool throttle function. probably won't use but just in case
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
 let ticking = false;
 window.addEventListener('scroll', function(e) {
     last_known_scroll_position = window.scrollY;
-   console.log(ticking );
-    if (!ticking) {
-        window.setTimeout(function() {
-            changeNavBar(last_known_scroll_position);
-            ticking = false;
-        }, 1000);
-    }
+    //console.log(ticking );
+        if (!ticking) {
+            window.setTimeout(function() {
+                changeNavBar(last_known_scroll_position);
+                ticking = false;
+            }, THROTTLE_DELAY);
+        }
   
     ticking = true;
 });
-*/
 
-window.addEventListener('scroll', function() {
-	if (
-		window.scrollY >= lastSelection.offsetTop - NAV_SELECT_BUFFER &&
-		window.scrollY <= lastSelection.offsetTop + lastSelection.offsetHeight - NAV_SELECT_BUFFER
+function changeNavBar(windY) {
+    if (
+		windY >= lastSelection.offsetTop - NAV_SELECT_BUFFER &&
+		windY <= lastSelection.offsetTop + lastSelection.offsetHeight - NAV_SELECT_BUFFER
 	) {
 		console.log('end');
 		return;
@@ -47,8 +47,8 @@ window.addEventListener('scroll', function() {
 	let num = 0;
 	for (const element of checkElements) {
 		num++;
-
-		if (window.scrollY < element.offsetTop + element.offsetHeight - NAV_SELECT_BUFFER) {
+        console.log(element.id + "- windY: " + windY + " offsetTop " + element.offsetTop + " offsetHeight " + element.offsetHeight);
+		if (windY < element.offsetTop + element.offsetHeight - NAV_SELECT_BUFFER) {
 			console.log('in ', element.id);
 			if (element != lastSelection) {
 				document.getElementById(element.id + '-nav').classList.add('active');
@@ -59,4 +59,33 @@ window.addEventListener('scroll', function() {
 			break;
 		}
 	}
-});
+}
+
+
+// window.addEventListener('scroll', function() {
+// 	if (
+// 		window.scrollY >= lastSelection.offsetTop - NAV_SELECT_BUFFER &&
+// 		window.scrollY <= lastSelection.offsetTop + lastSelection.offsetHeight - NAV_SELECT_BUFFER
+// 	) {
+// 		console.log('end');
+// 		return;
+// 	}
+// 	console.log('left ', lastSelection.id);
+// 	var checkElements = document.getElementsByClassName('section');
+
+// 	let num = 0;
+// 	for (const element of checkElements) {
+// 		num++;
+
+// 		if (window.scrollY < element.offsetTop + element.offsetHeight - NAV_SELECT_BUFFER) {
+// 			console.log('in ', element.id);
+// 			if (element != lastSelection) {
+// 				document.getElementById(element.id + '-nav').classList.add('active');
+// 				document.getElementById(lastSelection.id + '-nav').classList.remove('active');
+// 				lastSelection = element;
+// 			}
+// 			console.log('for loop times ', num);
+// 			break;
+// 		}
+// 	}
+// });
