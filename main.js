2f4aa33eadeb7c94d1e1afb120e3add9173babb0
lfriel <friellaura10@gmail.com>
2020-08-14 20:15:49 -0400

    highlight nav bar selection

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('js connected');
+
+var lastSelection = document.getElementById("home-nav");
+
+function changeNavSelect(selection) {
+    document.getElementById(selection).scrollIntoView();
+    let element = document.getElementById(selection+"-nav");
+    if (element != lastSelection) {
+        element.classList.add("active");
+        lastSelection.classList.remove("active");
+        lastSelection = element;
+    }
+}
