88e040d75c39e8634f21e321695ed301402f49a8
lfriel <friellaura10@gmail.com>
2020-08-14 22:28:45 -0400

    new highlight nav bar. currently very expensive and causes lag

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log('js connected');
 
 var lastSelection = document.getElementById("home-nav");
 
+/*
 function changeNavSelect(selection) {
     document.getElementById(selection).scrollIntoView();
     let element = document.getElementById(selection+"-nav");
@@ -11,3 +12,22 @@ function changeNavSelect(selection) {
         lastSelection = element;
     }
 }
+*/
+
+window.addEventListener("scroll", function() {
+    var checkElements = document.getElementsByClassName("section");
+	for (const element of checkElements) {
+        //console.log('section: ', element);
+        if (window.scrollY < (element.offsetTop) + 5) {
+            console.log("in ", element.id);
+            let navEelement = document.getElementById(element.id+"-nav");
+            if (navEelement != lastSelection) {
+                navEelement.classList.add("active");
+                lastSelection.classList.remove("active");
+                lastSelection = navEelement;
+            }
+            break;
+        }
+	}
+  });
+
