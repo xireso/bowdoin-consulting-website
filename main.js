dc7ec29af51d65f30f718018d5463c810031f524
lfriel <friellaura10@gmail.com>
2020-08-15 02:39:27 -0400

    fixed expensive funtion, shouldn't lag anymore. Edit buffer constant to change when page changes nav bar

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 console.log('js connected');
 
-var lastSelection = document.getElementById("home-nav");
+const NAV_SELECT_BUFFER = 20;
 
-/*
+var lastSelection = document.getElementById("home");
+
+/*//not needed bc new dynamic function
 function changeNavSelect(selection) {
     document.getElementById(selection).scrollIntoView();
     let element = document.getElementById(selection+"-nav");
@@ -14,18 +16,43 @@ function changeNavSelect(selection) {
 }
 */
 
+/*//cool throttle function. probably won't use but just in case
+//https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+let ticking = false;
+window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+   console.log(ticking );
+    if (!ticking) {
+        window.setTimeout(function() {
+            changeNavBar(last_known_scroll_position);
+            ticking = false;
+        }, 1000);
+    }
+  
+    ticking = true;
+});
+*/
+
 window.addEventListener("scroll", function() {
+    if (window.scrollY >= lastSelection.offsetTop - NAV_SELECT_BUFFER && window.scrollY <= lastSelection.offsetTop + lastSelection.offsetHeight - NAV_SELECT_BUFFER) {
+        console.log('end');
+        return;
+    }
+    console.log("left ", lastSelection.id);
     var checkElements = document.getElementsByClassName("section");
+
+    let num = 0;
 	for (const element of checkElements) {
-        //console.log('section: ', element);
-        if (window.scrollY < (element.offsetTop + element.offsetHeight / 1.75)) {
+        num++;
+        
+        if (window.scrollY < (element.offsetTop + element.offsetHeight - NAV_SELECT_BUFFER)) {
             console.log("in ", element.id);
-            let navEelement = document.getElementById(element.id+"-nav");
-            if (navEelement != lastSelection) {
-                navEelement.classList.add("active");
-                lastSelection.classList.remove("active");
-                lastSelection = navEelement;
+            if (element != lastSelection) {
+                document.getElementById(element.id+"-nav").classList.add("active");
+                document.getElementById(lastSelection.id+"-nav").classList.remove("active");
+                lastSelection = element;
             }
+            console.log('for loop times ', num);
             break;
         }
 	}
